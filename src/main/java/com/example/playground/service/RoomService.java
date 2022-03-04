ff886c5fe7946ed883fc796c6264d7cbb2ba1ee9
varot.tirakhuntorn <varotsk137@mail.kmutt.ac.th>
2022-03-04 17:55:08 +0700

    in progress - test mail template

--- src/main/java/com/example/playground/service/RoomService.java
+++ src/main/java/com/example/playground/service/RoomService.java
@@ -7,7 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 @Slf4j
 @Service
@@ -30,6 +33,20 @@ public class RoomService {
         mailSender.send(message);
         log.info("Send Email to message");
         return room;
+
+    }
+
+    @SneakyThrows
+    public void sendThemedMail(String to, String subject, String htmlBody){
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+        mailSender.send(message);
+        log.info("Send Email to message");
 
     }
 
